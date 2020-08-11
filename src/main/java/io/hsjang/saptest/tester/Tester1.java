a7f2e0e1e32aba83a856d3d16283cf0ec868475b
hsjang <admin@hsjang.io>
2020-08-11 18:04:54 +0900

    test

--- src/main/java/io/hsjang/saptest/tester/Tester1.java
+++ src/main/java/io/hsjang/saptest/tester/Tester1.java
@@ -29,8 +29,8 @@ public class Tester1 implements InitializingBean{
     // 전일 거래 정보
     List<Series> temp1=new ArrayList<Series>();
 
-    // 입력 정보
-    List<Series> series;
+    // 처리일자
+    Date dt;
 
     // 조건
     List<String> excludes = List.of("000000", "000001"); // 제외 symbol
@@ -47,6 +47,7 @@ public class Tester1 implements InitializingBean{
 
         try {
             this.balance = bal;
+            this.stocks = new HashMap<String,Stock>();
             Date startDt = new SimpleDateFormat("yyyyMMdd").parse(sDt);
             Date endDt = new SimpleDateFormat("yyyyMMdd").parse(eDt);
 
@@ -63,8 +64,9 @@ public class Tester1 implements InitializingBean{
             }
 
             while(dIdx>0 && openDays.get(dIdx).before(endDt)){
-                List<Series> candidates = seriesRepository.findByDateAndChangeGreaterThanEqual(openDays.get(dIdx), 0.29d);
-                testByDay(candidates);
+                Date dt = openDays.get(dIdx);
+                List<Series> candidates = seriesRepository.findByDateAndChangeGreaterThanEqual(dt, 0.29d);
+                testByDay(dt, candidates);
                 dIdx++;
             }
 
@@ -73,8 +75,9 @@ public class Tester1 implements InitializingBean{
         }
     }
 
-    public void testByDay(List<Series> daySeries){
-        this.series = daySeries;
+    public void testByDay(Date dt, List<Series> candidates){
+        this.dt = dt;
+
         List<Order> buyOrders = getBuyOrders();
         if(buyOrders.size()>0){
             buy(buyOrders);
@@ -86,13 +89,32 @@ public class Tester1 implements InitializingBean{
         }
 
         // 임시저장
-        temp1 = series;
+        temp1 = candidates;
     }
 
     // 후보(매수) 리스트
     public List<Order> getBuyOrders(){
-        return temp1.stream().filter(s->!excludes.contains(s.getSymbol()))
-            .map(s->{
+        // return temp1.stream().filter(s->!excludes.contains(s.getSymbol()))
+        //     .map(s->{
+        //         Long p = 0L;
+        //         if(!buyCondition.equals("i")){
+        //             String[] bCon = buyCondition.split(":");
+        //             if(bCon.length>1){
+        //                 if("p".equals(bCon[0])){
+        //                     int persent = Integer.parseInt(bCon[1]);
+        //                     p = s.getClose() * (1+persent/100);
+        //                 } 
+        //                 //else if{
+        //                     // else if others
+        //                 //}
+        //             }
+        //         }
+        //         return Order.ofBuy(s.getSymbol(), p);
+        //     }).collect(Collectors.toList());
+
+        List<Order> orders = new ArrayList<Order>();
+        for(Series s:temp1){
+            if(!excludes.contains(s.getSymbol())){
                 Long p = 0L;
                 if(!buyCondition.equals("i")){
                     String[] bCon = buyCondition.split(":");
@@ -106,46 +128,83 @@ public class Tester1 implements InitializingBean{
                         //}
                     }
                 }
-                return Order.ofBuy(s.getSymbol(), p);
-            }).collect(Collectors.toList());
+                orders.add(Order.ofBuy(s.getSymbol(), p));
+            }
+        }
+        return orders;
         // 전일 거래정보가 없거나 상한가 정보가 없으면 new ArrayList<Candidate>();  // size가 0이되면 그냥 temp1에 저장하고 종료
     }
 
     public List<Order> getSellOrders(){
-        return stocks.keySet().stream()
-            .map(s->{
-                Stock stock = stocks.get(s);
-                String[] pa = sellCondition.split(":");
-                int lp = Integer.parseInt(pa[0]);
-                int up = Integer.parseInt(pa[1]);
+        
+        // return stocks.keySet().stream()
+        //     .map(s->{
+        //         Stock stock = stocks.get(s);
+        //         String[] pa = sellCondition.split(":");
+        //         int lp = Integer.parseInt(pa[0]);
+        //         int up = Integer.parseInt(pa[1]);
 
-                Long uprice = stock.getPrice() * (1+up/100);
-                Long lprice = stock.getPrice() * (1+lp/100);
-                return Order.ofSell(stocks.get(s).getSymbol(), uprice, lprice);
-            }).collect(Collectors.toList());
+        //         Long uprice = stock.getPrice() * (1+up/100);
+        //         Long lprice = stock.getPrice() * (1+lp/100);
+        //         return Order.ofSell(stocks.get(s).getSymbol(), uprice, lprice);
+        //     }).collect(Collectors.toList());
+
+        List<Order> orders = new ArrayList<Order>();
+        for(String s: stocks.keySet()){
+            Stock stock = stocks.get(s);
+            String[] pa = sellCondition.split(":");
+            int lp = Integer.parseInt(pa[0]);
+            int up = Integer.parseInt(pa[1]);
+
+            Long uprice = stock.getPrice() * (1+up/100);
+            Long lprice = stock.getPrice() * (1+lp/100);
+            orders.add(Order.ofSell(stocks.get(s).getSymbol(), uprice, lprice));
+        }
+        return orders;
     }
 
     public void buy(List<Order> orders){
         // 매수
         // series : 오늘 정보
         Long max = balance * buyRatio / 100 / orders.size();
-        orders.stream().forEach(o->{
-            Series tEx = series.stream().filter(s->s.getSymbol().equals(o.getSymbol())).findFirst().get();
-            if(o.getPrice()==0L){
-                Long buyPrice = tEx.getOpen();
-                Long count = max/buyPrice;
-                
-                if(stocks.containsKey(o.getSymbol())){
-                    stocks.put(o.getSymbol(), stocks.get(o.getSymbol()).add(buyPrice, count));
-                }else{
-                    stocks.put(o.getSymbol(), new Stock(o.getSymbol(),buyPrice,count,tEx.getDate()));
+        // orders.stream().forEach(o->{
+        //     series.stream()
+        //         .filter(s->s.getSymbol().equals(o.getSymbol()))
+        //         .forEach(s->{
+        //             if(o.getPrice()==0L){
+        //                 Long buyPrice = s.getOpen();
+        //                 Long count = max/buyPrice;
+                        
+        //                 if(stocks.containsKey(o.getSymbol())){
+        //                     stocks.put(o.getSymbol(), stocks.get(o.getSymbol()).add(buyPrice, count));
+        //                 }else{
+        //                     stocks.put(o.getSymbol(), new Stock(o.getSymbol(),buyPrice,count,s.getDate()));
+        //                 }
+        //                 balance -= buyPrice*count;
+        //                 //System.out.println("BAL :: >>>>"+balance+" ["+s.getDate()+"]");
+        //             }
+        //         });
+        // });
+
+        for(Order o: orders){
+            Series s = seriesRepository.findByDateAndSymbol(dt,o.getSymbol());
+            if(s.getVolume()>0L){  // 거래량이 있을것.
+                if(o.getPrice()==0L){ // 시가 매수
+                    Long buyPrice = s.getOpen();
+                    Long count = max/buyPrice;
+                    
+                    if(stocks.containsKey(o.getSymbol())){
+                        stocks.put(o.getSymbol(), stocks.get(o.getSymbol()).add(buyPrice, count));
+                    }else{
+                        stocks.put(o.getSymbol(), new Stock(o.getSymbol(),buyPrice,count,dt));
+                    }
+                    balance -= buyPrice*count;
+                    //System.out.println("BAL :: >>>>"+balance+" ["+s.getDate()+"]");
                 }
             }
-        });
+        }
 
-        balance -= getTotalPrice();
-
-        System.out.println("BAL :: >>>>"+balance);
+        System.out.println(dt +"(bal:"+balance+")::>" + stocks);
     }
 
     public void sell(List<Order> orders){
@@ -155,8 +214,9 @@ public class Tester1 implements InitializingBean{
             
         // }
 
-        System.out.println(stocks);
-        System.out.println(balance);
+        System.out.println("SELL!!!");
+        // System.out.println(stocks);
+        // System.out.println(balance);
     }
 
     public Long getTotalPrice(){
