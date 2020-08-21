522431919892979048dec6b2ca48bcaffb3d5f58
hsjang <admin@hsjang.io>
2020-08-21 23:40:49 +0900

    test

--- src/main/java/io/hsjang/saptest/PageController.java
+++ src/main/java/io/hsjang/saptest/PageController.java
@@ -10,6 +10,6 @@ public class PageController {
 
     @RequestMapping(value="", method=RequestMethod.GET)
     public String main() throws Exception{
-        return "redirect:/main.html";
+        return "redirect:/index.html";
     }
 }
